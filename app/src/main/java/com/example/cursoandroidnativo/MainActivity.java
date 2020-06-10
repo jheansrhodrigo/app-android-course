11298c4886826d1f0368d22852deb792d78b9131
Jean Silva <jheanrds@gmail.com>
2020-06-09 23:57:23 -0300

    add main screen

--- app/src/main/java/com/example/cursoandroidnativo/MainActivity.java
+++ app/src/main/java/com/example/cursoandroidnativo/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.cursoandroidnativo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private String name;
+    private TextView startCourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        startCourse = findViewById(R.id.btn_start);
+        startCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DeveloperGuidesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
